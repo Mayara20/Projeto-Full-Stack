3144c43692cfec723b2bfacd3099eeb9a251f15b
Mayara Meireles <Mayarameirelessantos@gmail.com>
2022-09-10 14:44:36 -0300

    Atualização inicial

---
+++ Back/src/Cadastro.API/Cadastro.API/Controllers/EventoController.cs
@@ -0,0 +1,52 @@
+﻿using Cadastro.API.Data;
+using Cadastro.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class EventoController : ControllerBase
+	{
+		private readonly DataContext _context;
+
+		public EventoController(DataContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IEnumerable<Evento> Get()
+		{
+			return _context.Eventos;
+		}
+
+		[HttpGet("{id}")]
+		public Evento GetById(int id)
+		{
+			return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+		}
+
+		[HttpPost]
+		public string Post()
+		{
+			return "";
+		}
+		[HttpPut]
+		public string Put()
+		{
+			return "";
+		}
+		[HttpDelete]
+		public string Delete()
+		{
+			return "";
+		}
+	}
+}
---
+++ Back/src/Cadastro.API/Cadastro.API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Cadastro.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro.API.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{}
+
+		public DbSet<Evento> Eventos { get; set; }
+	}
+}
